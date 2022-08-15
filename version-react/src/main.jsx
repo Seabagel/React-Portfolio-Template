1ca30e98a8f78272350a411d2fc661d8f99537bb
Seabagel <hsrt1367tim@gmail.com>
2022-08-14 19:31:31 -0700

    Pass React Components as props
     Children of Article or other React components will now passed in as a prop instead of wrapped between their tags, unless needed

--- version-react/src/main.jsx
+++ version-react/src/main.jsx
@@ -11,16 +11,11 @@ export default function main({ children }) {
           imgURL={
             "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
           }
-          maxWidth={40}
-        >
-          <Section1 />
-        </ArticleImagebox>
-        <Article maxWidth={30}>
-          <Section1 />
-        </Article>
-        <Article>
-          <Section1 />
-        </Article>
+          maxWidth={30}
+          children={<Section1 />}
+        />
+        <Article maxWidth={25} children={<Section1 />} />
+        <Article children={<Section1 />} />
       </section>
 
       <section>
@@ -28,9 +23,8 @@ export default function main({ children }) {
           imgURL={
             "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
           }
-        >
-          <Section1 />
-        </ArticleImageSide>
+          children={<Section1 />}
+        />
       </section>
 
       <section className="reversed">
