e2c2cf4a7ea637bbe16cd4e09662425a91d0299a
Seabagel <hsrt1367tim@gmail.com>
2022-08-15 19:32:40 -0700

    Refactor CSS
    Separated code for footer and mobile from App.css

--- version-react/src/App.jsx
+++ version-react/src/App.jsx
@@ -3,6 +3,8 @@ import "./styles/App.css";
 import Main from "./Main";
 import Jumbotron from "./components/Jumbotron";
 import Footer from "./components/Footer";
+// always add mobile.css after everything
+import "./styles/mobile.css"
 
 export default function App() {
   return (
